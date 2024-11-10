916a8cc119de9ac5fbebf876909a669372be9499
Abdelrahman Khaled <66473152+abdelrahmankhal@users.noreply.github.com>
2024-11-10 20:52:14 +0300

    Create untitled2.py

---
+++ two/untitled2.py
@@ -0,0 +1,7 @@
+pressure = int ( input (" Enter pressure :  "))
+if pressure >80 :
+    print("lower pressure")
+elif pressure <20 :
+    print("increase pressure")
+else : 
+    print("pressure nominal")
