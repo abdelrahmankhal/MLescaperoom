f3d222c01b0b2db341f0ee3e097c411c4c60bc55
Abdelrahman Khaled <66473152+abdelrahmankhal@users.noreply.github.com>
2024-11-10 22:45:52 +0300

    Create untitled9.py

---
+++ 9/untitled9.py
@@ -0,0 +1,6 @@
+def transform ( data ) :
+
+# Sensor data with varying ranges
+ sensor_data = [500 , 1200 , 3000 , 8000]
+ normalized_data = transform ( sensor_data )
+ print ( f" Normalized data : {normalized_data}")
