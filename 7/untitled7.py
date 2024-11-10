1a65ed9cc4e2adc8ab575618aa16265583608fde
Abdelrahman Khaled <66473152+abdelrahmankhal@users.noreply.github.com>
2024-11-10 22:29:28 +0300

    Create untitled7.py

---
+++ 7/untitled7.py
@@ -0,0 +1,3 @@
+data = { 'homework_score': [40, 60, 80], 'exam_score': [70, 85, 90] }
+performance_index = [hw * exam for hw, exam in zip(data['homework_score'], data['exam_score'])]
+print(f"Performance Index (Homework * Exam): {performance_index}")
